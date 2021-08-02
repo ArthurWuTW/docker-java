39ea3789220f25c27e539a8bdbeb50250f1ebc62
ArthurWuTW <lion811004@gmail.com>
2021-08-02 20:46:24 +0800

    Change project name

--- docker/project/common.sh
+++ docker/project/common.sh
@@ -1,6 +1,6 @@
 REPO_DIR=$(realpath $(realpath $(dirname $PWD)/dependencies))
-HOSTNAME=wuguanlongs-MacBook-Pro
+HOSTNAME=arthur-MS-7B24
 DOCKER_DIR=$REPO_DIR
-IMAGE_NAME=${IMAGE_NAME:-project-java}
+IMAGE_NAME=${IMAGE_NAME:-project-java-tomcat}
 CONTAINER_USER=user
-CONTAINER_NAME=project-java
+CONTAINER_NAME=project-java-tomcat
